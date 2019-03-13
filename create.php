98a3051c8982302b9f0d4d290597e60edbca2822
ronakamilia27rpl <46396999+ronakamilia27rpl@users.noreply.github.com>
2019-03-13 11:08:43 +0700

    selesai dikerjakan

---
+++ create.php
@@ -0,0 +1,23 @@
+<?php
+
+include '../connect.php';
+
+$id_dosen = $_POST['id_dosen'];
+$nama_dosen = $_POST['nama_dosen'];
+$telp = $_POST['telp'];
+
+
+$query = "INSERT INTO dosen (id_dosen, nama_dosen, telp)
+            VALUES ('$id_dosen','$nama_dosen','$telp')";
+
+$result = mysqli_query($connect, $query);
+
+$num = mysqli_affected_rows($connect);
+
+    if($num > 0)
+    { 
+        echo "Berhasil tambah data";
+    } else {
+        echo "Gagal tambah data";
+    }
+     echo "<a href='read.php'>Lihat Data</a>";
---
+++ form-create.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<H1>Tambah Data Dosen</H1>
+</head>
+<body>
+    <form action="create.php" method="post">
+    
+    <p>No.         : <input type="text" name="id_dosen" > </p>
+    <p>Nama dosen : <input type="text" name="nama_dosen" ></p> 
+    <p>No. Telepon : <input type="text" name="telp" > </p>
+    <button name="btnSimpan">Simpan</button>
+    </form>
+</body>
+</html>
